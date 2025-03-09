7e75ef24172c38f6eaee6409c459b8c0a21d63bb
abu0127 <sr20010127@gmail.com>
2025-03-09 04:54:20 +0300

    ADD

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 function main() {
+    const firstText = 'Assalomu aleykum';
+    const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, ad.'
+    const targetDay = '1 chi fevral kuni soat 19:00';
+    const names = 'Simon riley and Sofia'
 
-    const contents = document
-        .querySelectorAll('.section');
-    const main = document
-        .querySelector('.content');
-    const txt_1 = document
-        .querySelector('.greating .cover');
-    const txt_2 = document
-        .querySelector('.greating2 .cover');
-    const txt_3 = document
-        .querySelector('.names .cover');
+    const contents = document.querySelectorAll('.section');
+    const main = document.querySelector('.content');
+    const profel = document.querySelector('.profel');
 
+    // text animation function
+    function writeTxt(txt, target, time) {
+        let writing = document.querySelector(target);
+        writing.textContent = '';
+
+        txt.split('').forEach((character, index) => {
+            let span = document.createElement('span');
+            span.textContent = character;
+            span.style.opacity = 0;
+            span.style.transition = 'opacity 1s ease'
+            writing.appendChild(span);
+
+            setTimeout(() => span.style.opacity = 1, time * index)
+        })
+    }
 
     //animations
-    setTimeout(() => {
-        main.style.marginTop = '400px'
-    }, 100);
-    setTimeout(() => {
-        txt_1.classList.add('txt_aninamtion_1')
-    }, 300);
-    setTimeout(() => {
-        txt_2.classList.add('txt_aninamtion_1')
-    }, 1200);
+    setTimeout(() => main.style.marginTop = '400px', 100);
+    // writing first text with animation
+    setTimeout(() => writeTxt(firstText, '.first-txt p', 30), 800);
+    // first desciription animation
+    setTimeout(() => writeTxt(description, '.description p', 20), 1800);
+    setTimeout(() => writeTxt(targetDay, '.target-day p', 20), 4000);
 
     window.addEventListener("scroll", function () {
         let scrollpos = window.scrollY;
-      
+
 
         contents.forEach((content, index) => {
-            let contentTop = content.offsetTop - 250;
+            let contentTop = content.offsetTop - 400;
             let contentHeight = content.offsetHeight;
 
             if (scrollpos >= contentTop && scrollpos < contentTop + contentHeight) {
 
-                if (index === 0) {
-                    txt_3.classList.add('txt_aninamtion_1')
+                if (index === 1) {
+                   profel.classList.add('profel-animated')
+                   setTimeout(() => writeTxt(names, '.name p', 30), 800);
+                   setTimeout(() => writeTxt(description, '.description-2 p', 20), 1400);
                 }
 
             }
@@ -43,4 +54,5 @@ function main() {
 
 
     });
+    console.log(contents)
 }
